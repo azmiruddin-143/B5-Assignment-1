e49beac5d57cccabbe118332c4581a7deb5cece2
rjazmirkhan <bangladeshfreelancerazmir@gmail.com>
2025-05-08 00:55:49 +0600

    Problem-4 Cpmplete

--- problem/problem.ts
+++ problem/problem.ts
@@ -38,7 +38,6 @@ const filterRating = filterByRating(books)
 console.log(filterRating);
 
 
-
 // Problem-3 
 
 function concatenateArrays<T>(...arrays: T[][]): T[] {
@@ -47,3 +46,52 @@ function concatenateArrays<T>(...arrays: T[][]): T[] {
 
 const allArayData = (concatenateArrays([1, 2], [3, 4], [5]))
 console.log(allArayData);
+
+
+// Problem-4
+
+
+class Vehicle {
+    private make:string;
+    private year: number;
+
+  constructor( make:string,year: number) {
+    this.make= make
+    this.year= year
+  }
+
+  getInfo() {
+     return (`Make ${this.make} Year ${this.year}`)
+  }
+
+  protected getMake() {
+    return this.make;
+  }
+  protected getYear() {
+    return this.year;
+  }
+
+
+}
+
+class Car extends Vehicle {
+
+    private model: string;
+
+   constructor( make:string,year: number, model:string) {
+    super(make,year);
+    this.model = model;
+   }
+ 
+   getModel() {
+      return (`Model ${this.model}`)
+   }
+
+   getFullInfo() { 
+    return (`Make: ${this.getMake} MOdel: ${this.model} Year: ${this.getYear}`)
+   }
+ }
+ 
+ const myCar = new Car("Toyota", 2020, "Corolla");
+
+ console.log(myCar);
