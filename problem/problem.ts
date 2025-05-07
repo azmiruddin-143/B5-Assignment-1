af823e715dbdf474082f9f7e99cecef89d60913c
rjazmirkhan <bangladeshfreelancerazmir@gmail.com>
2025-05-08 00:14:01 +0600

    Problem-2 Compelte

--- problem/problem.ts
+++ problem/problem.ts
@@ -1,15 +1,37 @@
+// Problem-1
 
 
-  function formatString  (typeing: string ,  toUpper?:boolean){
-
-    if(toUpper === true){
+function formatString(typeing: string, toUpper?: boolean) {
+    if (toUpper === true) {
         return typeing.toLocaleLowerCase()
     }
-    else  {
-     return typeing.toUpperCase()
+    else {
+        return typeing.toUpperCase()
     }
-       
-  }
 
-    const result = formatString("Hello", false)
-    console.log(result);
+}
+const result = formatString("Hello", false)
+// console.log(result);
+
+
+// problem-2//
+
+
+type Book = {
+    title: string,
+    rating: number
+}
+
+const books:Book[] = [
+    { title: "Book A", rating: 4.5 },
+    { title: "Book B", rating: 3.2 },
+    { title: "Book C", rating: 5.0 }
+];
+
+function filterByRating(items: Book[]):Book[] {
+    const filtering = items.filter(hi => hi.rating >= 4)
+    return filtering
+
+}
+
+console.log(filterByRating(books));
